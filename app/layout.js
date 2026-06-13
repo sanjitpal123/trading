1229e134a4e65e165422e8df6bafd9f9353a3558
sanjitssp <manjitpal895@gmail.com>
2026-06-13 22:38:26 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { ThemeProvider } from "./components/ThemeProvider";
 import FloatingContact from "./components/FloatingContact";
+import FixedBottomCTA from "./components/FixedBottomCTA";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
           <FloatingContact />
+          <FixedBottomCTA />
         </ThemeProvider>
       </body>
     </html>
